aa0f0067e3486209a368d42a53fd2db77603f4f0
Vincent <thisoneis4business@gmail.com>
2022-02-09 01:15:40 +0700

    Feat(web): Setting up for action cards

--- web/src/state/useAction.ts
+++ web/src/state/useAction.ts
@@ -4,6 +4,7 @@
 //
 //  Created by d-exclaimation on 16:25.
 //
+import { useEffect, useState } from "react";
 
 export type ActionCard = {
   type: "lock" | "fire" | "call" | "next" | "close";
@@ -11,4 +12,48 @@ export type ActionCard = {
   icon: string;
 };
 
-export const useAction = () => {};
+export function useAction() {
+  const [playerHand, setPlayerHand] = useState<ActionCard[]>([]);
+
+  useEffect(() => {
+    const hand = Array(5)
+      .fill(0)
+      .map(() => randomCard());
+    setPlayerHand(hand);
+  }, []);
+
+  return playerHand;
+}
+
+function randomCard(): ActionCard {
+  const i = Math.floor(Math.random() * 5);
+  const cardTypes: { [key: number]: ActionCard } = {
+    0: {
+      type: "fire",
+      color: "bg-violet-300",
+      icon: "🚀",
+    },
+    1: {
+      type: "lock",
+      color: "bg-blue-300",
+      icon: "🔭",
+    },
+    2: {
+      type: "call",
+      color: "bg-emerald-300",
+      icon: "📞",
+    },
+    3: {
+      type: "next",
+      color: "bg-red-300",
+      icon: "🚨",
+    },
+    4: {
+      type: "close",
+      color: "bg-yellow-300",
+      icon: "🪟",
+    },
+  };
+
+  return cardTypes[i];
+}
