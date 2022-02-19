6e00606f79016b41acf2d3916abf440da7ed015c
Vincent <thisoneis4business@gmail.com>
2022-02-19 13:19:27 +0700

    Feat(server): Opted for Elixir

---
+++ server/lib/spy_hunt.ex
@@ -0,0 +1,18 @@
+defmodule SpyHunt do
+  @moduledoc """
+  Documentation for `SpyHunt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpyHunt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ server/test/spy_hunt_test.exs
@@ -0,0 +1,8 @@
+defmodule SpyHuntTest do
+  use ExUnit.Case
+  doctest SpyHunt
+
+  test "greets the world" do
+    assert SpyHunt.hello() == :world
+  end
+end
