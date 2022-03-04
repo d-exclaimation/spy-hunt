233d1b3cab79f90f3107bf9d0f92cf49376b2caa
Vincent <thisoneis4business@gmail.com>
2022-03-04 20:44:03 +0700

    Fix(server): Fixed major firing and turn end logic issue

--- server/src/main/scala/actor/Party.scala
+++ server/src/main/scala/actor/Party.scala
@@ -27,6 +27,8 @@ object Party {
     case class Incoming(player: Client, action: InMessage) extends Act
 
     case class Quit(player: Client) extends Act
+
+    case class Decided(winner: Int) extends Act
   }
 
   case class State(
@@ -44,11 +46,11 @@ final class Party(context: ActorContext[Party.Act], player1: Client) extends Abs
   private var players: Players = Players.Waiting(player1)
 
   private val deck: mutable.ArrayDeque[Agent] = {
-    val neutrals = (1 to 10)
+    val neutrals = (1 to 2)
       .map(_ => Agent.neutral())
-    val player1s = (1 to 10)
+    val player1s = (1 to 2)
       .map(_ => Agent.player1())
-    val player2s = (1 to 10)
+    val player2s = (1 to 2)
       .map(_ => Agent.player2())
 
     mutable.ArrayDeque.from(Random.shuffle(neutrals ++ player1s ++ player2s))
@@ -65,20 +67,24 @@ final class Party(context: ActorContext[Party.Act], player1: Client) extends Abs
   private var isPlayer1Turn: Boolean = true
   private var moves: Int = 2
 
+
   def onMessage(msg: Act): Behavior[Act] = msg match {
     case Act.Join(player2) => same {
       players match {
         case Players.Waiting(player1) =>
+          val (p1, p2) = teamCount
+
           players = Players.Full(player1, player2)
 
           players.sendJson[OutMessage] { team =>
             OutMessage.Start(
               state = state(team),
-              allies = allyCount(team),
-              foes = foeCount(team),
+              allies = if (team == Agent.PLAYER1) p1 else p2,
+              foes = if (team == Agent.PLAYER1) p2 else p1,
               isTurn = if (team == Agent.PLAYER1) isPlayer1Turn else !isPlayer1Turn
             )
           }
+          logState()
 
         case Players.Full(_, _) =>
           ()
@@ -86,7 +92,6 @@ final class Party(context: ActorContext[Party.Act], player1: Client) extends Abs
     }
 
     case Act.Incoming(actingPlayer, action) => same {
-      // # TODO: Not Tested
       val isValid = players match {
         case Players.Waiting(_) => false
         case Players.Full(player1, player2) => actingPlayer.id match {
@@ -97,32 +102,21 @@ final class Party(context: ActorContext[Party.Act], player1: Client) extends Abs
       }
 
       if (isValid) {
-        action match {
-          case InMessage.IndexedAction("fire", index) => fire(index)
-          case InMessage.IndexedAction("lock", index) => lockOn(index)
-          case InMessage.IndexedAction("call", index) => call(index)
-          case InMessage.Action("shutter") => shutter()
-          case InMessage.Action("next") => next()
-          case _ => ()
-        }
-
-        players.sendJson[OutMessage] { team =>
-          OutMessage.Update(
-            state = state(team),
-            allies = allyCount(team),
-            foes = foeCount(team),
-            isTurn = if (team == Agent.PLAYER1) isPlayer1Turn else !isPlayer1Turn
-          )
-        }
-
-        moves -= 1
+        turn(action)
       }
 
-      if (moves <= 0) {
-        isPlayer1Turn = !isPlayer1Turn
-        moves = 2
-      }
+      logState()
     }
+
+    case Act.Decided(winner) =>
+      players.sendJson[OutMessage] { team =>
+        OutMessage.End(state(team),
+          win = winner == team,
+          reason = "Elimination of agents"
+        )
+      }
+      players.all.foreach(_.close())
+      Behaviors.stopped
 
     case Act.Status(ref) => same {
       ref ! players.isFull
@@ -144,16 +138,57 @@ final class Party(context: ActorContext[Party.Act], player1: Client) extends Abs
       }
   }
 
+
+  private def turn(action: InMessage): Unit = {
+    action match {
+      case InMessage.IndexedAction("fire", index) => fire(index)
+      case InMessage.IndexedAction("lock", index) => lockOn(index)
+      case InMessage.IndexedAction("call", index) => call(index)
+      case InMessage.Action("shutter") => shutter()
+      case InMessage.Action("next") => next()
+      case _ => ()
+    }
+
+    moves -= 1
+
+    if (moves <= 0) {
+      isPlayer1Turn = !isPlayer1Turn
+      moves = 2
+      next()
+    }
+
+    val (p1, p2) = teamCount
+    val isDecided = p1 == 0 || p2 == 0
+
+    players.sendJson[OutMessage] { team =>
+      OutMessage.Update(state(team),
+        allies = if (team == Agent.PLAYER1) p1 else p2,
+        foes = if (team == Agent.PLAYER1) p2 else p1,
+        isTurn = team == Agent.PLAYER1 && isPlayer1Turn && moves > 1
+      )
+    }
+
+    if (isDecided) {
+      context.self ! Act.Decided(
+        if (p1 == 0) Agent.PLAYER2 else Agent.PLAYER1
+      )
+    }
+  }
+
   // MARK: - Computed State
 
   private def state(team: Int): Seq[Window.WithAgent] =
     windows.zip(queue).toSeq.map { case (w, a) => w.and(a.perspective(team)) }
 
-  private def allyCount(team: Int): Int =
-    (deck ++ queue).count(_.team == team)
+  private def teamCount: (Int, Int) =
+    (deck ++ queue).toSeq.map(_.team).foldLeft((0, 0)) { (count, team) =>
+      team match {
+        case Agent.PLAYER1 => (count._1 + 1, count._2)
+        case Agent.PLAYER2 => (count._1, count._2 + 1)
+        case _ => count
+      }
+    }
 
-  private def foeCount(team: Int): Int =
-    (deck ++ queue).count(a => a.team != team && a.team != Agent.NEUTRAL)
 
   // MARK: - Actions
 
@@ -164,21 +199,22 @@ final class Party(context: ActorContext[Party.Act], player1: Client) extends Abs
   }
 
   private def lockOn(i: Int): Unit = {
-    if (i < 0 || i > 4) return
+    if (i < 0 || i > 4) return;
     val prev = windows(i)
     windows.update(i, Window(isOpen = prev.isOpen, isTargeted = true))
   }
 
   private def fire(i: Int, quick: Boolean = false): Unit = {
-    if (i < 0 || i > 4) return
-    if (!quick && !windows(i).isTargeted) return
+    if (i < 0 || i > 4) return;
+    if (!quick && !windows(i).isTargeted) return;
+
     shift(i)
     windows.update(i, Window())
   }
 
 
   private def call(i: Int): Unit = {
-    if (i < 0 || i > 4) return
+    if (i < 0 || i > 4) return;
 
     val saved = queue(i)
     shift(i)
@@ -193,9 +229,10 @@ final class Party(context: ActorContext[Party.Act], player1: Client) extends Abs
   }
 
   private def shift(i: Int): Unit = {
-    if (i < 0 || i > 4) return
+    if (i < 0 || i > 4) return;
     val next = if (deck.nonEmpty) deck.removeHead() else Agent.neutral()
-    if (i < 1) {
+
+    if (i > 0) {
       for (j <- (1 to i).reverse) {
         queue.update(j, queue(j - 1))
       }
@@ -204,6 +241,18 @@ final class Party(context: ActorContext[Party.Act], player1: Client) extends Abs
   }
 
   // MARK: - Utils
+
+  private def logState(): Unit = {
+    val (p1, p2) = teamCount
+    println(s"------------------------------")
+    println(s"p1: $p1                p2: $p2")
+    println(s"------------------------------")
+    println(s"${queue.map(a => s" (${a.team}) ").mkString("-")}")
+    println(s"${windows.map(w => if (w.isTargeted) " [x] " else " [ ] ").mkString("-")}")
+    println(s"------------------------------")
+    println(s"${deck.map(a => s" (${a.team}) ").mkString("~")}")
+    println(s"------------------------------")
+  }
 
   private def same(on: => Unit): Party = {
     on
